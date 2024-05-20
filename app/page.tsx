7ec4d794c545def10ff0bacf980007bc1fb34876
Roy Lopez <ryhsu2@gmail.com>
2024-05-20 11:47:54 -0300

    chore: Update Supabase connection initialization in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Index() {
     }
   };
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  const isSupabaseConnected = P();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
