c5b106d20267e7eb4a39dc3194f4cf0293960b8a
Roy Lopez <ryhsu2@gmail.com>
2024-05-24 12:16:47 -0300

    Update contactFormValidationSchema with name length validation messages in Spanish

--- schemas/contactFormValidationSchema.ts
+++ schemas/contactFormValidationSchema.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 export const contactFormValidationSchema = z.object({
-  name: z.string({
-    message: "El nombre no puede estar vacío",
-  }),
+  name: z
+    .string({
+      message: "Por favor, introduce tu nombre",
+    })
+    .min(2, "El nombre debe tener al menos 2 caracteres")
+    .max(20, "El nombre no puede tener más de 20 caracteres"),
   email: z
     .string({
       message: "El email no es válido",
@@ -13,7 +16,7 @@ export const contactFormValidationSchema = z.object({
     .string({
       message: "El mensaje no puede estar vacío",
     })
-    .max(200),
+    .max(300, "El mensaje no puede tener más de 300 caracteres"),
 });
 
 export type ContactFormValues = z.infer<typeof contactFormValidationSchema>;
