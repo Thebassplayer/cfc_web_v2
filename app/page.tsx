12408f9a78522a5a540033c747ea2586e00e02ab
Roy Lopez <ryhsu2@gmail.com>
2024-05-27 12:50:41 -0300

    chore: Update layout and HeroOne components
    This commit updates the layout and HeroOne components. It adjusts the grid row height in layout.tsx for better screen display and updates HeroOne.tsx to use different images for desktop and mobile views.

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import HeroTwo from "@/components/HeroTwo/HeroTwo";
 
 export default function Home() {
   return (
-    <main className="row-start-2 row-end-13 h-full overflow-y-auto">
+    <>
       <HeroOne />
       <HeroTwo />
-    </main>
+    </>
   );
 }
