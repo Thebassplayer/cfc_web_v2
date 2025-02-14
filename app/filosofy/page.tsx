68683d0ed1e6325c527a409190c25eb1a7cd12bd
Roy Lopez <ryhsu2@gmail.com>
2025-02-14 19:43:27 +0100

    feat: Remove unused pages and components from the application

--- app/filosofy/page.tsx
+++
@@ -1,9 +0,0 @@
-const Filosofia = () => {
-  return (
-    <div className="flex h-full w-full grow flex-col items-center justify-center gap-20">
-      <h1 className="font-sifonn text-4xl">Filosofia</h1>
-    </div>
-  );
-};
-
-export default Filosofia;
