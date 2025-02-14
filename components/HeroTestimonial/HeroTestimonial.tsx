c0855aec960672447b116fd2ab7f6a20d8cded32
Roy Lopez <ryhsu2@gmail.com>
2025-02-14 11:07:53 +0100

    fix: Adjust layout and styling of HeroTestimonial component for improved presentation

--- components/HeroTestimonial/HeroTestimonial.tsx
+++ components/HeroTestimonial/HeroTestimonial.tsx
@@ -2,11 +2,11 @@ import TestimonialsCarousell from "../TestimonialsCarousell/TestimonialsCarousel
 
 export const HeroTestimonial = () => {
   return (
-    <section className="flex h-3/4 flex-col bg-grey-light p-4 lg:flex lg:flex-col lg:px-28">
-      <div className="mx-auto my-8 h-2 w-1/4 bg-primary-yellow"></div>
-      <h1 className="col-span-2 mb-8 text-center font-sifonn text-4xl leading-none text-purple-primary lg:text-5xl">
+    <section className="flex h-full flex-col bg-grey-light p-4 lg:flex lg:flex-col lg:px-28">
+      <h1 className="col-span-2 mt-8 text-center font-sifonn text-4xl leading-none text-purple-primary lg:text-5xl">
         TESTIMONIOS
       </h1>
+      <div className="mx-auto mb-8 h-2 w-1/4 bg-primary-yellow"></div>
       <div className="flex grow flex-col items-center justify-center">
         <TestimonialsCarousell />
       </div>
