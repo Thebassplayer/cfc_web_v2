32d456b068a9c875a098a3f4e696fc2f755eb610
Roy Lopez <ryhsu2@gmail.com>
2025-02-14 19:06:40 +0100

    feat: Refactor ContactButton to support customizable sizes and improved styling

--- components/ContactButton/ContactButton.tsx
+++ components/ContactButton/ContactButton.tsx
@@ -1,4 +1,5 @@
 import { APP_ROUTES } from "@/constants/routes";
+import { cm } from "@/utils/classMerge";
 import Link from "next/link";
 import React from "react";
 
@@ -7,14 +8,23 @@ type ContactButtonProps = {
   size?: "sm" | "md" | "lg";
 };
 
+const sizes = {
+  sm: "text-lg px-4 py-2",
+  md: "text-xl px-6 py-3",
+  lg: "text-2xl px-8 py-4",
+};
+
 const ContactButton = ({ text, size = "sm" }: ContactButtonProps) => {
   return (
-    <Link
-      href={APP_ROUTES.CONTACT}
-      title="Quiero saber mas"
-      className="w-full rounded-md bg-primary-yellow px-4 py-2 text-center font-roboto font-semibold text-purple-dark transition-transform hover:scale-105 active:bg-purple-dark active:text-primary-yellow lg:mt-4 lg:justify-self-center "
-    >
-      {text}
+    <Link href={APP_ROUTES.CONTACT} title={text}>
+      <div
+        className={cm(
+          "w-fit rounded-md bg-primary-yellow text-center font-roboto font-semibold text-purple-dark transition-transform hover:scale-105 active:bg-purple-dark active:text-primary-yellow",
+          sizes[size],
+        )}
+      >
+        {text}
+      </div>
     </Link>
   );
 };
