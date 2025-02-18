139653d348d6cfffbb0c83e4cd9e15bd535ea9ea
Roy Lopez <ryhsu2@gmail.com>
2025-02-18 14:55:53 +0100

    fix: Adjust layout and typography in HeroEstudiaAnatomia component for improved readability and responsiveness

--- components/HeroEstudiaAnatomia/HeroEstudiaAnatomia.tsx
+++ components/HeroEstudiaAnatomia/HeroEstudiaAnatomia.tsx
@@ -1,14 +1,14 @@
 export const HeroEstudiaAnatomia = () => {
   return (
-    <section className="grid grid-rows-3 p-4 lg:h-3/4 lg:grid-cols-2 lg:grid-rows-2 lg:px-28 lg:py-14">
-      <div className="row-start-1 flex flex-col justify-evenly lg:col-start-1 lg:pb-14">
+    <section className="flex flex-col p-4 lg:grid lg:h-3/4 lg:grid-cols-2 lg:grid-rows-2 lg:px-28 lg:py-14">
+      <div className="row-start-1 flex flex-col justify-evenly pb-8 lg:col-start-1 lg:pb-14">
         <h1 className="font-sifonn text-3xl leading-none text-purple-primary lg:text-4xl">
           “Estudia anatomía y transforma tu conexión cuerpo-mente con CFC
           Conciencia y Flexibilidad Corporal®”
         </h1>
         <div className="my-2 h-2 w-2/5 bg-primary-yellow"></div>
       </div>
-      <div className="row-span-2 row-start-2 flex flex-col justify-evenly py-2 lg:col-start-1 lg:col-end-3 lg:flex-row lg:justify-center">
+      <div className="row-span-2 row-start-2 flex flex-col gap-8 py-2 lg:col-start-1 lg:col-end-3 lg:flex-row lg:justify-center">
         <div className="flex flex-col justify-evenly lg:block lg:w-1/2 lg:px-4">
           <h1 className="font-bebas text-2xl leading-none text-purple-light lg:pb-4">
             “Construye una base fuerte y flexible con nuestro método”
@@ -26,14 +26,14 @@ export const HeroEstudiaAnatomia = () => {
           <h1 className="font-bebas text-2xl leading-none text-purple-light lg:pb-4">
             ¿Qué es y para qué sirve?
           </h1>
-          <p className="mb-4 text-sm">
+          <p className="mb-4 text-xs lg:text-base">
             Conciencia y Flexibilidad Corporal ® (CFC) es un método que toma
             recursos de varias disciplinas del movimiento y el autoconocimiento.
             Fue creado para acompañar a las personas en el desarrollo del
             registro propioceptivo, la flexibilidad, la fuerza y la
             auto-consciencia.
           </p>
-          <p className="text-sm">
+          <p className="text-xs lg:text-base">
             Esta metodología contribuye al incremento de las capacidades en los
             términos antes mencionados, además de prevenir y acompañar a los
             cuerpos en sus procesos (véase prevención de riesgos lesivos,
