d2208edd9055ec0dc387c78bfb45c6b72670cba8
Roy Lopez <ryhsu2@gmail.com>
2025-02-18 14:29:39 +0100

    feat: Implement testimonial carousel with navigation controls in HeroAQuienEstaDirigida component

--- components/HeroAQuienEstaDirigida/HeroAQuienEstaDirigida.tsx
+++ components/HeroAQuienEstaDirigida/HeroAQuienEstaDirigida.tsx
@@ -1,7 +1,13 @@
+"use client";
 import HeroThreeCard from "../HeroThreeCard/HeroThreeCard";
 import Lautaro from "@/assets/Lautaro.jpg";
 import Ajustes from "@/assets/ajuste.jpg";
 import AlbuMili from "@/assets/albu-mili-1.jpg";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const cards = [
   {
@@ -23,21 +29,54 @@ const cards = [
   },
 ];
 
+const variants = {
+  enter: (direction: number) => ({
+    opacity: 0,
+    x: direction > 0 ? 100 : -100,
+  }),
+  center: {
+    opacity: 1,
+    x: 0,
+  },
+  exit: (direction: number) => ({
+    opacity: 0,
+    x: direction > 0 ? -100 : 100,
+  }),
+};
+
 export const HeroAQuienEstaDirigida = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState(1);
+
+  const nextTestimonial = () => {
+    setDirection(1);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
+  };
+
+  const prevTestimonial = () => {
+    setDirection(-1);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + cards.length) % cards.length,
+    );
+  };
   return (
-    <section className="h-full grid-rows-3 bg-grey-light p-4 lg:grid lg:grid-cols-3 lg:grid-rows-5 lg:px-28 lg:py-14">
-      <div className="row-start-1 flex flex-col lg:col-start-1 lg:pb-10">
-        <h1 className="my-0 font-sifonn text-4xl leading-none text-purple-primary lg:text-5xl">
+    <section className="flex h-full flex-col bg-grey-light p-4 lg:grid lg:grid-cols-3 lg:grid-rows-5 lg:px-28 lg:py-14">
+      <div className="row-start-1 hidden flex-col text-5xl lg:col-start-1 lg:flex lg:pb-10">
+        <h1 className="my-0 font-sifonn  leading-none text-purple-primary">
           ¿A
           <span className="text-primary-yellow"> quien</span>
         </h1>
-        <h1 className="my-0 font-sifonn text-4xl leading-none text-purple-primary lg:text-5xl">
+        <h1 className="my-0 font-sifonn leading-none text-purple-primary">
           esta dirigida
         </h1>
-        <h1 className="my-0 font-sifonn text-4xl leading-none text-purple-primary lg:text-5xl">
+        <h1 className="my-0 font-sifonn leading-none text-purple-primary">
           la practica?
         </h1>
       </div>
+      <h1 className="my-0 font-sifonn text-3xl leading-none text-purple-primary lg:hidden">
+        ¿A <span className="text-primary-yellow"> quien</span> esta dirigida la
+        practica?
+      </h1>
       <div className="row-span-4 row-start-2 hidden justify-evenly py-2 lg:col-start-1 lg:col-end-4 lg:flex lg:flex-row lg:justify-center">
         {cards.map((card) => (
           <HeroThreeCard
@@ -47,6 +86,75 @@ export const HeroAQuienEstaDirigida = () => {
             text={card.text}
           />
         ))}
+      </div>
+      <div className="flex grow flex-col items-center justify-between lg:hidden">
+        <div className="relative grow">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={currentIndex}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{ duration: 0.5 }}
+              className="lg:flex"
+            >
+              <div className="mx-10 flex flex-1 grow flex-col pt-4 lg:mx-14">
+                <Image
+                  src={cards[currentIndex].image}
+                  alt={cards[currentIndex].title}
+                  className="mb-8 aspect-square w-full rounded-2xl object-cover shadow-md shadow-black"
+                />
+                <div className="flex grow flex-col justify-between">
+                  <div>
+                    <h2 className="font-bebas text-xl leading-none text-purple-light lg:pb-4 lg:text-2xl">
+                      {cards[currentIndex].title}
+                    </h2>
+                    <p className="overflow-y-auto text-xs lg:text-sm">
+                      {cards[currentIndex].text}
+                    </p>
+                  </div>
+                  <div>
+                    <Link
+                      href={"?search-modal=true"}
+                      title="Quiero saber mas"
+                      className="text-purple-light hover:underline"
+                    >
+                      Mas Info
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+          <button
+            onClick={prevTestimonial}
+            className="absolute left-0 top-1/2 -translate-y-1/2 transform rounded-full bg-purple-primary p-2 text-white transition-transform hover:scale-105 active:scale-100"
+            aria-label="Previous testimonial"
+          >
+            <ChevronLeft size={24} />
+          </button>
+          <button
+            onClick={nextTestimonial}
+            className="absolute right-0 top-1/2 -translate-y-1/2 transform rounded-full bg-purple-primary p-2 text-white transition-transform hover:scale-105 active:scale-100"
+            aria-label="Next testimonial"
+          >
+            <ChevronRight size={24} />
+          </button>
+        </div>
+        <div className="mt-8 flex justify-center space-x-2">
+          {cards.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`h-3 w-3 rounded-full ${
+                index === currentIndex ? "bg-purple-primary" : "bg-gray-300"
+              }`}
+              aria-label={`Go to testimonial ${index + 1}`}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
