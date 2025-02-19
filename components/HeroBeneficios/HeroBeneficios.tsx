c04e02353871e19732df05da7424a52222673331
Roy Lopez <ryhsu2@gmail.com>
2025-02-19 10:53:20 +0100

    fix: Adjust text styling and layout in Hero components for improved readability

--- components/HeroBeneficios/HeroBeneficios.tsx
+++ components/HeroBeneficios/HeroBeneficios.tsx
@@ -13,10 +13,10 @@ export const HeroBeneficios = () => {
       </div>
       <div className="row-span-2 row-start-2 flex flex-col justify-evenly gap-8 py-2 lg:col-start-1 lg:col-end-3 lg:flex-row lg:justify-center lg:gap-0">
         <div className="flex flex-col justify-evenly lg:block lg:w-1/3 lg:px-4">
-          <h1 className="font-bebas text-2xl leading-none text-purple-light lg:pb-4">
+          <h1 className="pb-4 font-bebas text-2xl leading-none text-purple-light">
             Beneficios físicos
           </h1>
-          <ul>
+          <ul className="text-xs md:text-base">
             <li>1. Mejora de la flexibilidad </li>
             <li>2. Incremento de la fuerza</li>
             <li>3. Desarrollo de habilidades</li>
@@ -26,10 +26,10 @@ export const HeroBeneficios = () => {
           </ul>
         </div>
         <div className="flex flex-col justify-evenly lg:block lg:w-1/3 lg:px-4">
-          <h1 className="font-bebas text-2xl leading-none text-purple-light lg:pb-4">
+          <h1 className="pb-4 font-bebas text-2xl leading-none text-purple-light">
             Beneficios mentales y emocionales
           </h1>
-          <ul>
+          <ul className="text-xs md:text-base">
             <li>1. Aumento de la autoconciencia y autoconfianza</li>
             <li>2. Reducción del estrés y ansiedad</li>
             <li>3. Mejora de la concentración</li>
@@ -37,10 +37,10 @@ export const HeroBeneficios = () => {
           </ul>
         </div>
         <div className="flex flex-col justify-evenly lg:block lg:w-1/3 lg:px-4">
-          <h1 className="font-bebas text-2xl leading-none text-purple-light lg:pb-4">
+          <h1 className="pb-4 font-bebas text-2xl leading-none text-purple-light">
             Beneficios integrales
           </h1>
-          <ul>
+          <ul className="text-xs md:text-base">
             <li>1. Conexión Mente-Cuerpo</li>
             <li>
               2. Desarrollo personal y profesional en caso de realizar la
