7df77712f665dedaa47f74c4d4486ba2d7f68f1a
Roy Lopez <ryhsu2@gmail.com>
2025-03-24 20:40:17 +0100

    fix: Adjust height and text styles in ConoceBanner for improved layout and responsiveness

--- components/ConoceBanner/ConoceBanner.tsx
+++ components/ConoceBanner/ConoceBanner.tsx
@@ -5,7 +5,7 @@ import ContactButton from "../ContactButton/ContactButton";
 
 const ConoceBanner = () => {
   return (
-    <div className="relative flex h-[600px] items-center justify-center overflow-hidden">
+    <div className="relative flex h-[400px] items-center justify-center overflow-hidden lg:h-[600px]">
       <Image
         src={ConoceYExpandeTuFuerza}
         alt="Mujer en Flecha"
@@ -15,17 +15,20 @@ const ConoceBanner = () => {
         priority
       />
       <div className="relative z-10 flex h-full w-full flex-col justify-end px-4 py-8 text-white sm:px-6 lg:px-8">
-        <h1 className="shadow-text font-sifonn font-bold leading-6 lg:text-5xl">
+        <h1 className="shadow-text  font-sifonn text-3xl font-bold lg:text-5xl lg:leading-6">
           Conoce y expande
         </h1>
-        <h1 className="shadow-text font-sifonn font-bold lg:text-5xl">
+        <h1 className="shadow-text  font-sifonn text-3xl font-bold lg:text-5xl">
           tu fuerza y flexibilidad
         </h1>
-        <p className="shadow-text mb-8 font-bebas text-xl lg:text-2xl">
+        <p className="shadow-text  mb-8 font-bebas text-xl lg:text-2xl">
           Formate como instructor/a o en carácter personal
         </p>
-        <div className="space-x-4">
+        <div className="hidden space-x-4 lg:flex">
           <ContactButton text={"Comenzar"} size="md" />
+        </div>
+        <div className="space-x-4 lg:hidden">
+          <ContactButton text={"Comenzar"} />
         </div>
       </div>
     </div>
