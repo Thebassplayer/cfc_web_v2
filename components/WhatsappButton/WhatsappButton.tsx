86d96fa792c9ffb14c1b4b4f6f7746afcb09f056
Roy Lopez <ryhsu2@gmail.com>
2025-03-25 21:19:47 +0100

    fix: Update WhatsappButton URL to use a static message link

--- components/WhatsappButton/WhatsappButton.tsx
+++ components/WhatsappButton/WhatsappButton.tsx
@@ -6,17 +6,11 @@ type WhatsappButtonProps = {
   children: React.ReactNode;
 };
 
-if (!process.env.NEXT_PUBLIC_PHONE_NUMBER) {
-  throw new Error("NEXT_PUBLIC_PHONE_NUMBER is not defined");
-}
-
-const phoneNumber = process.env.NEXT_PUBLIC_PHONE_NUMBER;
-
 const WhatsappButton = ({ children }: WhatsappButtonProps) => {
   const messageText = encodeURIComponent(
     "Hola, quisiera recibir mas informacion sobre el metodo CFC",
   );
-  const url = `https://wa.me/${phoneNumber}?text=${messageText}`;
+  const url = `https://wa.me/message/R3VJ7UUOCORRO1`;
 
   return (
     <Link href={url} target="_blank">
