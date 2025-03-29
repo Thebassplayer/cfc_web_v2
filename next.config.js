26f5ddaaff54a27a07113f13d21a3fc0f7224b1a
Roy Lopez <ryhsu2@gmail.com>
2025-03-29 16:04:16 +0100

    fix: Set custom distribution directory for landing page build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { hostname } = require("os");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "dist/landing-cfc",
   images: {
     remotePatterns: [{ hostname: "lh3.googleusercontent.com" }],
   },
